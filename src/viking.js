e6b09534265a3c8d3969bbf6c3528f60b52df8aa
Sara Silva <saracarolinasilva@gmail.com>
2022-10-07 12:12:35 +0200

    iteration_2 done

--- src/viking.js
+++ src/viking.js
@@ -18,8 +18,19 @@ class Viking extends Soldier {
     super(health, strength);
     this.name = name;
   }
-}
+  receiveDamage(damage) {
+    let newHealth = (this.health -= damage);
 
+    if (newHealth > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else if (newHealth <= 0) {
+      return `${this.name} has died in act of combat`;
+    }
+  }
+  battleCry() {
+    return `Odin Owns You All!`;
+  }
+}
 // Saxon
 class Saxon {}
 
